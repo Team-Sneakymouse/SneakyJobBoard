b884b813f089a0a01c515c7da8ed306ef48e8971
MomoPewpew <50401575+MomoPewpew@users.noreply.github.com>
2024-04-26 10:01:11 +0200

    Further updates to webhook util
    face image is now a 72x72
    inner layer upscaled 8 times to 64x64
    outer layer upscaled 9 times to 72x72
    
    webhook now supports a location display string, that supports placeholders

--- src/main/kotlin/net/sneakyjobboard/util/WebhookUtility.kt
+++ src/main/kotlin/net/sneakyjobboard/util/WebhookUtility.kt
@@ -1,6 +1,7 @@
 package net.sneakyjobboard.util
 
 import java.awt.Graphics2D
+import java.awt.Image
 import java.awt.image.BufferedImage
 import java.io.ByteArrayOutputStream
 import java.net.ConnectException
@@ -48,14 +49,29 @@ object WebhookUtility {
 
     /** Converts a Job into a json-ready map. */
     private fun createJobDataData(job: Job): Map<String, Any> {
+        // Poster display string
         var posterStringWebhook =
-                (SneakyJobBoard.getInstance().getConfig().getString("poster-string-webhook")
+                (SneakyJobBoard.getInstance().getConfig().getString("webhook-poster")
                                 ?: "[playerName]").replace("[playerName]", job.player.name)
 
         if (SneakyJobBoard.isPapiActive()) {
             posterStringWebhook = PlaceholderAPI.setPlaceholders(job.player, posterStringWebhook)
         }
 
+        // Location display string
+        var locationStringWebhook =
+                (SneakyJobBoard.getInstance().getConfig().getString("webhook-location")
+                                ?: "[x],[y],[z]")
+                        .replace("[x]", job.location.blockX.toString())
+                        .replace("[y]", job.location.blockY.toString())
+                        .replace("[z]", job.location.blockZ.toString())
+
+        if (SneakyJobBoard.isPapiActive()) {
+            locationStringWebhook =
+                    PlaceholderAPI.setPlaceholders(job.player, locationStringWebhook)
+        }
+
+        // Base64 face icon
         val skinURL = job.player.playerProfile.textures.skin
 
         val faceIconBase64 =
@@ -74,6 +90,7 @@ object WebhookUtility {
                 "poster" to posterStringWebhook,
                 "posterIcon" to faceIconBase64,
                 "location" to job.location.toString(),
+                "locationDisplayString" to locationStringWebhook,
                 "startTime" to job.startTime,
                 "durationMilis" to job.durationMilis,
                 "name" to job.name,
@@ -96,15 +113,30 @@ object WebhookUtility {
      * skin.
      */
     private fun createPlayerIcon(skin: BufferedImage): BufferedImage {
-        val icon = BufferedImage(16, 16, BufferedImage.TYPE_INT_ARGB)
+        val icon = BufferedImage(72, 72, BufferedImage.TYPE_INT_ARGB)
         val g: Graphics2D = icon.createGraphics()
 
-        val face = skin.getSubimage(8, 8, 8, 8)
-        val helmet = skin.getSubimage(40, 8, 8, 8)
+        // Scale the face image to fit the inner layer of the icon
+        val scaledFace = skin.getSubimage(8, 8, 8, 8).getScaledInstance(64, 64, Image.SCALE_DEFAULT)
+        val faceBufferedImage = BufferedImage(64, 64, BufferedImage.TYPE_INT_ARGB)
+        faceBufferedImage.createGraphics().apply {
+            drawImage(scaledFace, 0, 0, null)
+            dispose()
+        }
+        g.drawImage(faceBufferedImage, 4, 4, null)
 
-        g.drawImage(face, 0, 0, null)
-        if (!isFullyTransparent(helmet)) {
-            g.drawImage(helmet, 0, 0, null)
+        // Scale the helmet layer to match the dimensions of the icon and overlay it on top of the
+        // scaled face
+        val scaledHelmet =
+                skin.getSubimage(40, 8, 8, 8).getScaledInstance(72, 72, Image.SCALE_DEFAULT)
+        val helmetBufferedImage = BufferedImage(72, 72, BufferedImage.TYPE_INT_ARGB)
+        helmetBufferedImage.createGraphics().apply {
+            drawImage(scaledHelmet, 0, 0, null)
+            dispose()
+        }
+		
+        if (!isFullyTransparent(helmetBufferedImage)) {
+            g.drawImage(helmetBufferedImage, 0, 0, null)
         }
 
         g.dispose()
@@ -117,13 +149,14 @@ object WebhookUtility {
                 (0 until image.height).any { y -> (image.getRGB(x, y) ushr 24) != 0 }
             }
 
-    /** Encode an image to a base64 String. */
+    /** Encode an image to a base64 String with proper prefix. */
     private fun encodeImageToBase64(image: BufferedImage): String {
         val outputStream = ByteArrayOutputStream()
         return try {
             ImageIO.write(image, "png", outputStream)
             val imageBytes = outputStream.toByteArray()
-            Base64.getEncoder().encodeToString(imageBytes)
+            val base64String = Base64.getEncoder().encodeToString(imageBytes)
+            "data:image/png;base64,$base64String"
         } finally {
             outputStream.close()
         }
